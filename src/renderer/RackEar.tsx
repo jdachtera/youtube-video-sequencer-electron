7974952feb9e7449d27b10a4ef3151733801f244
chris <chris@pyracom.de>
2022-02-02 16:57:17 +0100

    small css refactor

--- src/renderer/RackEar.tsx
+++ src/renderer/RackEar.tsx
@@ -1,31 +1,38 @@
-import ScrewHeadWithHole from '../../assets/svg/screw_head_with_hole.svg';
 import { css } from '@emotion/css';
+import ScrewHeadWithHole from '../../assets/svg/screw_head_with_hole.svg';
+
+const rackEarStyle = css`
+  box-shadow: 0px 0px 4px white;
+  border: 3px outset #ffffff !important;
+  background: rgb(254, 243, 241) !important;
+  background: radial-gradient(
+    circle,
+    rgb(255, 255, 255) 0%,
+    #e2e2e2 80%
+  ) !important;
+`;
+
+const screwStyle = css`
+  margin: 8px;
+`;
 
 const RackEar = () => {
-  return ( 
-  <div
-    className={css`
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      // border-right: 1px inset #222;
-      // border-bottom: 1px outset #777;
-      box-shadow: 0px 0px 2px #222;
-      `}>
+  return (
+    <div className={rackEarStyle}>
       <img
         alt="screw"
         src={ScrewHeadWithHole}
         width="35px"
-        style={{ margin: '8px' }}
+        style={screwStyle}
       />
       <img
         alt="screw"
         src={ScrewHeadWithHole}
         width="35px"
-        style={{ margin: '8px' }}
+        style={screwStyle}
       />
-  </div>)
-}
+    </div>
+  );
+};
 
 export default RackEar;
-
